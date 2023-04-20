37b6166b4d87031c7580b61778e1dfd647164fd6
KamilNIegowski <kamil@KamilN>
2023-04-20 22:44:19 +0200

    creation of Currency support

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,6 @@
     
     // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
     use Illuminate\Database\Seeder;
-    use Illuminate\Support\Facades\DB;
     
     class DatabaseSeeder extends Seeder
     {
@@ -13,15 +12,11 @@
          */
         public function run(): void
         {
-            DB::table( 'personal_access_tokens' )->insert( [
-                'tokenable_id' => 1, // ID użytkownika, który będzie miał dostęp do API, w tym przypadku 1
-                'tokenable_type' => 'KRGroup', // Nazwa modelu użytkownika
-                'name' => 'TokenAuth', // Nazwa tokenu
-                'token' => 'token_autoryzacyjny', // Token autoryzacyjny
-                'abilities' => '["*"]', // Uprawnienia tokenu, w tym przypadku wszystkie
-                'last_used_at' => now(), // Ostatnie użycie tokenu, w tym przypadku teraz
-                'created_at' => now(), // Data utworzenia tokenu
-                'updated_at' => now(), // Data ostatniej aktualizacji tokenu
-            ] );
+            // \App\Models\User::factory(10)->create();
+            
+            // \App\Models\User::factory()->create([
+            //     'name' => 'Test User',
+            //     'email' => 'test@example.com',
+            // ]);
         }
     }
