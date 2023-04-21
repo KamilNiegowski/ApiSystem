debdd79b86a791cdcdd723105c070b293342222c
KamilNIegowski <kamil@KamilN>
2023-04-21 18:51:52 +0200

    create table user and token

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -20,6 +20,13 @@ return new class extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+        DB::table('users')->insert([
+            'name' => 'KRGroup',
+            'email' => 'admin@krgroup.pl',
+            'password' => bcrypt('krgroup123'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
