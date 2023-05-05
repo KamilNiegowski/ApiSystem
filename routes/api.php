37ddceb31bde8b3bfa47260f06abfc83805ddbe7
KamilNIegowski <kamil@KamilN>
2023-05-05 22:52:20 +0200

    final commit

--- routes/api.php
+++ routes/api.php
@@ -1,32 +1,25 @@
 <?php
-    
-    use App\Http\Controllers\Api\AuthController;
-    use App\Http\Controllers\Api\CurrencyController;
-    use Illuminate\Support\Facades\Route;
-    
-    /*
-    |--------------------------------------------------------------------------
-    | API Routes
-    |--------------------------------------------------------------------------
-    |
-    | Here is where you can register API routes for your application. These
-    | routes are loaded by the RouteServiceProvider and all of them will
-    | be assigned to the "api" middleware group. Make something great!
-    |
-    */
-    
-    // Route group dla autoryzowanych użytkowników
-    Route::middleware( 'auth:sanctum' )->group( function () {
-        
-        // Endpoint dla pobrania kursów walut
-        Route::get( '/currencies', [ CurrencyController::class, 'index' ] );
-        
-        // Endpoint dla dodania kursu waluty
-        Route::post( '/currencies', [ CurrencyController::class, 'store' ] )
-            ->middleware( 'once-per-day' );
-        
-    } );
 
-// Endpoint dla autoryzacji tokenu
-    Route::post( '/login', [ AuthController::class, 'login' ] );
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CurrencyController;
+use Illuminate\Support\Facades\Route;
 
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('currencies', CurrencyController::class);
+    Route::get('currencies/{currency}/{date}', [CurrencyController::class, 'showByCurrencyAndDate']);
+    Route::get('currencies-date/{date}', [CurrencyController::class, 'showByDate']);
+    Route::get('currencies-today/{currency}', [CurrencyController::class, 'showByCurrency']);
+});
